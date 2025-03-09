3463e0fff53c8567d3e3846eb45945e86df65847
snehal <snehal.m.tambe@gmail.com>
2025-03-09 20:57:57 +0530

    March 9th, 2025 first code to be pushed on Git

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic;
 
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
     }
 }
