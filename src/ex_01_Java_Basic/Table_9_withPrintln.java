eed6c778acf57d43bd0d0b3cd831cd3f023b1496
snehal <snehal.m.tambe@gmail.com>
2025-03-11 11:22:41 +0530

    March 11th, 2025 Printing table of 9 using println

---
+++ src/ex_01_Java_Basic/Table_9_withPrintln.java
@@ -0,0 +1,30 @@
+package ex_01_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Table_9_withPrintln {
+    public static void main(String[] args) {
+        int num = 9;
+        int num1 = 9*2;
+        int num2 = 9*3;
+        int num3 = 9*4;
+        int num4 = 9*5;
+        int num5 = 9*6;
+        int num6 = 9*7;
+        int num7 = 9*8;
+        int num8 = 9*9;
+        int num9 = 9*10;
+
+        System.out.println("Table of num: " +num);
+        System.out.println("Table of num: " +num1);
+        System.out.println("Table of num: " +num2);
+        System.out.println("Table of num: " +num3);
+        System.out.println("Table of num: " +num4);
+        System.out.println("Table of num: " +num4);
+        System.out.println("Table of num: " +num5);
+        System.out.println("Table of num: " +num6);
+        System.out.println("Table of num: " +num7);
+        System.out.println("Table of num: " +num8);
+        System.out.println("Table of num: " +num9);
+    }
+}
