3ef0cd1d909a667bfb46489564e379df17626107
snehal <snehal.m.tambe@gmail.com>
2025-03-11 11:06:31 +0530

    March 11th, 2025 Printing table of 9 using printf

---
+++ src/ex_01_Java_Basic/table_9_with_printf.java
@@ -0,0 +1,27 @@
+package ex_01_Java_Basic;
+
+public class table_9_with_printf {
+    public static void main(String[] args) {
+        int a1 = 9;
+        int a2 = 9*2;
+        int a3 = 9*3;
+        int a4 = 9*4;
+        int a5 = 9*5;
+        int a6 = 9*6;
+        int a7 = 9*7;
+        int a8 = 9*8;
+        int a9 = 9*9;
+        int a10 = 9*10;
+        System.out.printf("Your variable is %d",a1);
+        System.out.printf("Your variable is %d",a2);
+        System.out.printf("Your variable is %d",a3);
+        System.out.printf("Your variable is %d",a4);
+        System.out.printf("Your variable is %d",a5);
+        System.out.printf("Your variable is %d",a6);
+        System.out.printf("Your variable is %d",a7);
+        System.out.printf("Your variable is %d",a8);
+        System.out.printf("Your variable is %d",a9);
+        System.out.printf("Your variable is %d",a10);
+
+    }
+}
