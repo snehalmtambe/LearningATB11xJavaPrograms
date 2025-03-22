e360eace04611cde35f996de92a170c3ba5e36a3
snehal <snehal.m.tambe@gmail.com>
2025-03-22 16:41:20 +0530

    March 11th, 2025 Printing table of 9 using println

--- src/ex_03Java_Operators/Task_12th_march.java
+++ src/ex_03Java_Operators/Task_12th_march.java
@@ -1,6 +1,6 @@
 package ex_03Java_Operators;
 
-import static jdk.internal.foreign.abi.riscv64.RISCV64Architecture.Regs.x2;
+//import static jdk.internal.foreign.abi.riscv64.RISCV64Architecture.Regs.x2;
 
 public class Task_12th_march {
     public static void main(String[] args) {
@@ -9,7 +9,25 @@ public class Task_12th_march {
         int x = 10;
         int y = 10;
         int z = 10;
-        long result;
+
+
+        double x_sq = Math.pow(x,2);
+        double y_sq = Math.pow(y,2);
+
+        double z_abs = Math.abs(z);
+
+        double result = x_sq+y_sq-z_abs;
+
+        double result_cr = Math.cbrt(result);
+
+        System.out.println("Value of X square is :" +x_sq);
+        System.out.println("Value of Y square is :" +y_sq);
+        System.out.println("Value of z absolute is:" +z);
+
+        System.out.println("Value of the result is: " +result);
+
+        System.out.println("Value of cube root of complete result is: " +result_cr);
+
 
         //result = (x2 + y2 - |z|);
 
