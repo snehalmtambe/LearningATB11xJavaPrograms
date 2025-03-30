4db028821b6808546d2d6820a8458920a9611684
snehal <snehal.m.tambe@gmail.com>
2025-03-30 20:40:04 +0530

    Typecast

---
+++ src/Task_14th_march/Lab_006_even_odd.java
@@ -0,0 +1,23 @@
+package Task_14th_march;
+
+import java.util.Scanner;
+
+public class Lab_006_even_odd {
+    public static void main(String[] args) {
+        System.out.println("Please enter number");
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a%2==0)
+        {
+            System.out.println("Enter number is even");
+        }
+        else
+        {
+            System.out.println("Enter number is odd");
+        }
+
+
+    }
+}
