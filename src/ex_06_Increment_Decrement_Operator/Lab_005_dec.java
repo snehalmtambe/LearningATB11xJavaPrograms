2e2d977c41047a134e09091d4aded4e25569d73e
snehal <snehal.m.tambe@gmail.com>
2025-03-30 20:29:33 +0530

    Typecast

---
+++ src/ex_06_Increment_Decrement_Operator/Lab_005_dec.java
@@ -0,0 +1,11 @@
+package ex_06_Increment_Decrement_Operator;
+
+public class Lab_005_dec
+{
+    public static void main(String[] args)
+    {
+            int a = 20;
+            System.out.println(--a + a++ + a--);
+    }
+
+}
