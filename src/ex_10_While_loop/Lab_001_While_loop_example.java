87fcfe4be830ad290b072d02a3996ae7fa8cb584
snehal <snehal.m.tambe@gmail.com>
2025-04-06 21:57:47 +0530

    while loop

---
+++ src/ex_10_While_loop/Lab_001_While_loop_example.java
@@ -0,0 +1,15 @@
+package ex_10_While_loop;
+
+public class Lab_001_While_loop_example {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while(i<10)
+        {
+            System.out.println("Value of i is:" +i);
+            i++;
+        }
+
+    }
+
+}
