3a1cce36397d5c6423901211735bfc7a75125b62
snehal <snehal.m.tambe@gmail.com>
2025-04-12 12:41:16 +0530

    List of vowels and consonent

--- src/Task_21st_March/Lab_Count_Vowels.java
+++ src/Task_21st_March/Lab_Count_Vowels.java
@@ -3,14 +3,11 @@ package Task_21st_March;
 public class Lab_Count_Vowels {
     public static void main(String[] args) {
         String name = "Pramod";
-        int vovels = 0;
+        int vowels = 0;
         int consonent = 0;
-
         name = name.toLowerCase();
-
         int count = name.length();
         char ch;
-
         for(int i=0; i<count; i++)
         {
             ch = name.charAt(i);
@@ -18,20 +15,14 @@ public class Lab_Count_Vowels {
             if(Character.isLetter(ch))
             {
                 if(ch=='a' || ch=='e' || ch=='i' || ch=='o' ||ch=='u') {
-                    vovels++;
-
+                    vowels++;
                 }
                 else {
                     consonent++;
-
                 }
-
-
-
             }
         }
-        System.out.println("Wovel present in string are:"+vovels);
+        System.out.println("Wovel present in string are:"+vowels);
         System.out.println("Consonent present in string are:"+consonent);
-
     }
 }
