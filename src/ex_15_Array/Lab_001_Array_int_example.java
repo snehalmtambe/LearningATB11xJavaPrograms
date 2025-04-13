d81e07fe416f453cfe6becfafeb28da1d5398fc6
snehal <snehal.m.tambe@gmail.com>
2025-04-13 13:25:27 +0530

    Array examples

---
+++ src/ex_15_Array/Lab_001_Array_int_example.java
@@ -0,0 +1,18 @@
+package ex_15_Array;
+
+public class Lab_001_Array_int_example {
+    public static void main(String[] args) {
+        System.out.println("This is the Array example");
+        int [] a = {51,62,48,93,1,28};
+        int b[] = {41,63,67,84,96};
+
+        System.out.println("Integer length of a is: "+a.length);
+        System.out.println("2nd element of array a is:"+a[1]);
+
+        System.out.println("Integer length of b is: "+b.length);
+        System.out.println("second element of array b is:"+b[3]);
+
+
+
+    }
+}
---
+++ src/ex_15_Array/Lab_002_Array_String_example.java
@@ -0,0 +1,17 @@
+package ex_15_Array;
+
+public class Lab_002_Array_String_example {
+    public static void main(String[] args) {
+        System.out.println("This is the example of String array");
+
+        String [] str1 = new String[3];
+        str1[0] = "Naman";
+        str1[1] = "Vikas";
+        str1[2] = "Niel";
+
+        System.out.println("Length of String array is :"+str1.length);
+        System.out.println("First element on the string array is:"+str1[0]);
+        System.out.println("last element on the string array is:"+str1[2]);
+
+    }
+}
